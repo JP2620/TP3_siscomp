c7fc2d28cae660386567447194e7941d2a640a31
Juan Pablo Saucedo <saucedojuanpablo@gmail.com>
2021-06-14 01:44:10 +0100

    Lee de /dev/siscom

--- src/server.c
+++ src/server.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <ulfius.h>
 #include <jansson.h>
@@ -6,15 +8,38 @@
 #define PORT 8537
 #define PREFIX "/api"
 
-int callback_get_data(const struct _u_request *request, 
-    struct _u_response *response, void *user_data)
+int callback_get_data(__attribute__((unused)) const struct _u_request *request, 
+    struct _u_response *response, __attribute__((unused)) void *user_data)
 {
+  int retval;
   json_t * body_res = json_object();
+  char medicion[20];
+  int fd_dev = open("/dev/siscom", O_RDONLY);
+  if (fd_dev == -1)
+  {
+    json_object_set(body_res, "descripcion", json_string("/dev/siscom "
+          "no disponible"));
+    ulfius_set_json_body_response(response, 503, body_res);
+    perror("open");
+    return U_CALLBACK_ERROR;
+  }
+  retval = read(fd_dev, medicion, sizeof(medicion));
+  if (retval == -1)
+  {
+    json_object_set(body_res, "descripcion", json_string("/dev/siscom "
+          "no disponible"));
+    ulfius_set_json_body_response(response, 503, body_res);
+    perror("read");
+    return U_CALLBACK_ERROR;
+  }
+  close(fd_dev);
   json_object_set(body_res, "medicion", json_integer(rand() % 20));
   ulfius_set_json_body_response(response, MHD_HTTP_OK, body_res);
   return U_CALLBACK_CONTINUE;
 }
 
+int callback_set_sensor(const struct _u_request *request, 
+    struct _u_response *response, void *user_data);
 
 int main()
 {
@@ -31,6 +56,9 @@ int main()
   // Endpoints
   ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/data", 0,
                              &callback_get_data, NULL);
+  ulfius_add_endpoint_by_val(&instance, "POST", PREFIX, "/data", 0,
+                             &callback_get_data, NULL);
+
 
   // Inicializar el framework
   retval = ulfius_start_framework(&instance); // Abre conexion http
